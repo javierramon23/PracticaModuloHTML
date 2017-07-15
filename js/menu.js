60f663b7948646f41b2c5c779ff15de36759e5db
Javier Ramón <javierramon@outlook.com>
2017-07-15 20:20:59 +0200

    Modificamos el script del smoothScroll para hacerlo compatible con todos los navegadores

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 // Se "almacenan" los elementos del menu principal en una variable para trabajar con ellos.
 var mainMenuItems = document.getElementsByClassName("main-menu-item");
-
+console.log(mainMenuItems);
 // Se crea un bucle que va a recorrer cada uno de los elementos del menu principal.
 for (var i = 0; i < mainMenuItems.length; i++) {
     // Para cada uno de esos elementos se añade un evento que se ejecutara al hacer click sobre ellos.
@@ -16,22 +16,25 @@ for (var i = 0; i < mainMenuItems.length; i++) {
 }
 
 function getElementByIdAndScroll(id) {
+    console.log("getElementByIdAndScroll");
     var element;
-
     if (element === "") {
         element = document.getElementsByClassName("header")[0];
     }else{
         element = document.getElementById(id);
     }
-
+console.log(element);
     scrollToElement(element);
 }
 
 function scrollToElement(element) {
+    console.log("ScrollToElement");
     var jump = parseInt(element.getBoundingClientRect().top * 0.3);
-
-    document.body.scrollTop += jump;
-
+    console.log(jump)
+    document.documentElement.scrollTop +=  jump;
+    document.body.scrollTop +=  jump;
+    window.pageYOffset += jump;
+    console.log(document.documentElement.scrollTop);
     if (!element.lastJump || element.lastJump > Math.abs(jump)) {
         element.lastJump = Math.abs(jump);
         setTimeout(function() {
